d7f4de250719b7e3817bf326481fde19350a35f9
Geoff English <geoffrey.english@uqconnect.edu.au>
2020-02-21 07:36:04 +1000

    finished the student table

--- create_student_table.sql
+++ create_student_table.sql
@@ -8,8 +8,8 @@ CREATE TABLE student (
   sat_score integer,
   programOfStudy varchar(50),
   -- Need to make the rcd columns a timestamp. This tutorial will help: https://www.postgresqltutorial.com/postgresql-timestamp/
-  rcd_Created TIMESTAMPTZ,
-  rcd_Updated TIMESTAMPTZ
+  rcd_Created timestamptz,
+  rcd_Updated timestamptz
 )
 
 -- CREATE TABLE Persons (
@@ -20,19 +20,59 @@ CREATE TABLE student (
 -- CHECK (Age>=18) -- This ensures everyone records their age at 18 years old or older
 -- );
 
-insert into student (
-  studentID, FirstName, LastName, sat_score, programOfStudy, rcd_Created, rcd_Updated
-) values ( 
+INSERT INTO student (
+  studentid, firstname, lastname, sat_score, programofstudy, rcd_created, rcd_updated
+) VALUES ( 
   1, 'Monica', 'Davis', 400, 'Literature', now(), now()
-)
+);
 
-insert into student (
-  studentID, FirstName, LastName, sat_score, programOfStudy, rcd_Created, rcd_Updated
-) values ( 
+INSERT INTO student (
+  studentid, firstname, lastname, sat_score, programofstudy, rcd_created, rcd_updated
+) VALUES ( 
   2, 'Teri', 'Gutierrez', 800, 'Programming', now(), now()
-) values ( 
+);
+
+INSERT INTO student (
+  studentid, firstname, lastname, sat_score, programofstudy, rcd_created, rcd_updated
+) VALUES ( 
   3, 'Spencer', 'Pautier', 1000, 'Programming', now(), now()
-)
+);
+
+INSERT INTO student (
+  studentid, firstname, lastname, sat_score, programofstudy, rcd_created, rcd_updated
+) VALUES ( 
+4, 'Louis', 'Ramsey',1200, 'Programming', now(), now()
+);
+
+INSERT INTO student (
+  studentid, firstname, lastname, sat_score, programofstudy, rcd_created, rcd_updated
+) VALUES ( 
+5, 'Alvin', 'Greene',1200, 'Programming', now(), now()
+);
+
+INSERT INTO student (
+  studentid, firstname, lastname, sat_score, programofstudy, rcd_created, rcd_updated
+) VALUES ( 
+6, 'Sophie', 'Freeman',1200, 'Programming', now(), now()
+);
+
+INSERT INTO student (
+  studentid, firstname, lastname, sat_score, programofstudy, rcd_created, rcd_updated
+) VALUES ( 
+7, 'Edgar Frank "Ted"', 'Codd',2400, 'Computer Science', now(), now()
+);
+
+INSERT INTO student (
+  studentid, firstname, lastname, sat_score, programofstudy, rcd_created, rcd_updated
+) VALUES ( 
+8, 'Donald D.', 'Chamberlin',2400, 'Computer Science', now(), now()
+);
+
+INSERT INTO student (
+  studentid, firstname, lastname, sat_score, programofstudy, rcd_created, rcd_updated
+) VALUES ( 
+9, 'Raymond F.', 'Boyce',2400, 'Computer Science', now(), now()
+);
 
 
 
